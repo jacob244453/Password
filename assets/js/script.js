37734642911958a44b60b84a841e9c23aee64fe5
jacob244453 <85600569+jacob244453@users.noreply.github.com>
2021-07-11 17:46:15 -0400

    finished touching up the js
    received values through the checkboxes on the page, and generate password using character codes, matching up with what the user requires.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
-const characterAmountNumber = document.getElementById
-  ('characterAmuountNumber')
+//////////assignment through html id
+const characterAmount = document.getElementById
+  ('characterAmountNumber')
 const includeUppercaseElement = document.getElementById
   ('includeUppercase')
 const includeNumbersElement = document.getElementById
@@ -7,16 +8,46 @@ const includeNumbersElement = document.getElementById
 const includeSymbolsElement = document.getElementById
   ('includeSymbols')
 const form = document.getElementById('passwordGeneratorForm')
+const passwordDisplay = document.getElementById('passwordDisplay')
 
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47)
+
+
+
+//////////get values from page
 form.addEventListener('submit', e => {
-e.preventDefault()
-const characterAmountNumber = characterAmountNumber.value
-const includeUppercase = includeUppercaseElement.checked
-const includeNumbers = includeNumbersElement.checked
-const includeSymbols = includeSymbolsElement.checked
-const password = generatePassword(characterAmountNumber, includeUppercase, includeNumbers, includeSymbols )
+  e.preventDefault()
+  const characterAmount = characterAmountNumber.value
+  const includeUppercase = includeUppercaseElement.checked
+  const includeNumbers = includeNumbersElement.checked
+  const includeSymbols = includeSymbolsElement.checked
+  const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
+  passwordDisplay.innerText = password
 })
 
-function generatePassword(characterAmountNumber, includeUppercase, includeNumbers, includeSymbols){
-  String.fromCharCode()
+//////////generate password using character codes
+function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
+  let charCodes = LOWERCASE_CHAR_CODES
+  if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
+  if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+  if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
+
+  const passwordCharacters = []
+  for (let i = 0; i < characterAmount; i++) {
+    const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+    passwordCharacters.push(String.fromCharCode(characterCode))
+  }
+  return passwordCharacters.join('')
+}
+
+/////////funvtion to generate arrays
+function arrayFromLowToHigh(low, high) {
+  const array = []
+  for (let i = low; i <= high; i++) {
+    array.push(i)
+  }
+  return array
 }
